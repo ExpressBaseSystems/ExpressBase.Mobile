c592b85f8fa632da7133c9bec617ef13add9e8c1
Amal K Dev <amaldev@expressbase.com>
2020-07-27 16:46:21 +0530

    minor

---
+++ Configuration/VendorSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Mobile.Configuration
+{
+    public static class EbSettings
+    {
+        public const string VendorName = "ExpressBase";
+
+        public const string AppIcon = "@mipmap/icon_eb";
+
+        public const string AppLabel = "EXPRESSbase";
+
+        public const string StatusBar = "#0046bb";
+
+        public const string SplashTheme = "@style/Theme.Splash";
+    }
+
+    public static class MoSettings
+    {
+        public const string VendorName = "MoveOn";
+
+        public const string AppIcon = "@mipmap/icon_mo";
+
+        public const string AppLabel = "Move On";
+
+        public const string StatusBar = "#283b97";
+
+        public const string SplashTheme = "@style/Theme.SplashMO";
+    }
+}
